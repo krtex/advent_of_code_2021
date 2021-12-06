a8b42655a71158e0f8667ac5e4b1d0d7959ac3dc
Michał <krtexxx@gmail.com>
2021-12-06 21:38:32 +0100

    Day 3, part 1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,20 +4,31 @@ fn part1()
         .map(|line| line.parse().unwrap())
         .collect::<Vec<String>>();
     
-    let gamma: [i16; 12] = [0; 12];
+    let mut counts: [i16; 12] = [0; 12];
 
     for line in &input
     {
-        for (i, c) in line.enumerate()
+        for (i, c) in line.chars().enumerate()
         {
             match c
             {
-                "0" => gamma[i] -= 1,
-                "1" => gamma[i] += 1,
+                '0' => counts[i] -= 1,
+                '1' => counts[i] += 1,
+                _ => counts[i] += 0,
             } 
         }
-
     }
+    
+    let mut gamma = 0;
+    for i in 0..counts.len()
+    {
+        if counts[i] > 0
+        {
+            gamma +=  1 << (11 - i);
+        }
+    }
+    println!("Gamma = {}, gamma*epsilon = {}",
+    gamma, gamma * (!gamma & ((1 << 12) - 1)));
 }
 
 fn main()
